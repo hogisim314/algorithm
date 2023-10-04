dd717604bd1830fcc195faa87fb54288516104cc
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2023-10-04 22:28:19 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,29 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int hp[30];
-int joy[30];
-int dp[30][130];
+
+int n;
+int hp[22];
+int happy[22];
+int dp[22][111]; //dp[i][j] 에서 i명까지 봤을 때, 체력을 j까지 쓰는것 체력이 총 100까지 담을 수 있는 것. 
 int main() {
-	int n;
-	cin >> n;
-	int max_hp = 100;
-	for (int i = 1; i <= n; i++) {
-		cin >> hp[i];
-	}
-	for (int i = 1; i <= n; i++) {
-		cin >> joy[i];
-	}
-	int maxx = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j < 100; j++) {
-			if (j - hp[i] < 0) {
-				dp[i][j] = dp[i - 1][j];
-			}
-			else {
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - hp[i]] + joy[i]);
-			}
-			maxx = max(maxx, dp[i][j]);
-		}
-	}
-	cout << maxx;
+    cin>>n;
+    for (int i=1; i<=n; i++) {
+        cin>>hp[i];
+    }
+
+    for (int i=1; i<=n; i++) {
+        cin>>happy[i];
+    }
+
+    for (int i=0; i<=n; i++) {
+        for (int j=0; j<100; j++) {
+            if (i==0 || j==0) {
+                dp[i][j] = 0;
+            }
+            else {
+                if (hp[i]<=j) {
+                    if (dp[i-1][j]<=dp[i-1][j-hp[i]]+happy[i]) {
+                        dp[i][j] = dp[i-1][j-hp[i]] + happy[i];
+                    } else {
+                        dp[i][j] = dp[i-1][j];
+                    }
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+    }
+    cout<<dp[n][99];
 }
