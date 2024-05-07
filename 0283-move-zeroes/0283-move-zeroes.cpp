5fd9446036c8359e2d2a331a474713e413f19bbd
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2024-05-07 14:30:16 +0900

    Time: 11 ms (91.03%), Space: 21.4 MB (95.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int ZeroIdx = -1; //가장 왼쪽의 0을 가리키는
-        int nonZeroIdx = 0; // 안바뀐 숫자들의 인덱스를 가지는
-        while(true) {
-            if (nonZeroIdx == nums.size()) {
-                break;
+        int i = 0;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                swap(nums[j], nums[i]);
+                i++;
             }
-            
-            else if (nums[nonZeroIdx]!=0) {
-                for (int i=0; i<nonZeroIdx; i++) {
-                    if (nums[i]==0) {
-                        ZeroIdx=i;
-                        break;
-                    }
-                }
-                if (ZeroIdx==-1) {
-                    nonZeroIdx++;
-                    continue;
-                }
-                nums[ZeroIdx]=nums[nonZeroIdx];
-                nums[nonZeroIdx] = 0;
-                
-            }
-            nonZeroIdx++;
         }
     }
 };
