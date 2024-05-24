0d03f61c63932e47b5e33e14e9e98bad0280beb4
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2024-05-24 16:04:49 +0900

    Time: 15 ms (68.21%), Space: 21.7 MB (37.13%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        for (int j = 0; j < nums.size(); j++) {
-            if (nums[j] != 0) {
-                swap(nums[j], nums[i]);
-                i++;
+        int zeroIdx = 0;
+        bool found = false;
+        for (int i=0; i<nums.size(); i++) {
+            if (!found && nums[i]==0) {
+                zeroIdx = i;
+                found = true;
+            }
+            else if (nums[i]!=0) {
+                if (found) {
+                    nums[zeroIdx] = nums[i];
+                    nums[i] = 0;
+                    found = true;
+                    for (int j=zeroIdx+1; j<nums.size(); j++) {
+                        if (nums[j]==0) {
+                            zeroIdx = j;
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
