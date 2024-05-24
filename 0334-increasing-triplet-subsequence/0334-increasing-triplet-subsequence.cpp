84bbebdfde21ac43b6c6c8830d0f0a729649e68f
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2024-05-24 15:51:49 +0900

    Time: 92 ms (29.6%), Space: 114.3 MB (29.51%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    bool increasingTriplet(vector<int>& nums) 
-    {
-        int first_small = INT_MAX;
-        int second_small = INT_MAX;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]<=first_small)
-                first_small=nums[i];
-            else if( nums[i]<= second_small )
-                second_small= nums[i];
-            else
+    bool increasingTriplet(vector<int>& nums) {
+        int min1=INT_MAX, min2 = INT_MAX;
+
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i]<=min1) {
+                min1 = nums[i];
+            }
+            else if (nums[i]<=min2) {
+                min2 = nums[i];
+            }
+            else if (nums[i]>min1 && nums[i]>min2) {
                 return true;
+            }
         }
         return false;
     }
