a5ad9d4033d7f3dddb01889b696d6bc6ac7676e1
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2024-05-24 14:40:26 +0900

    Time: 51 ms (98.03%), Space: 21.8 MB (95.16%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -4,52 +4,32 @@ public:
         if (word1.size()!=word2.size()) {
             return false;
         }
-        else {
-            set<char> s1, s2;
-            map<char, int> m1, m2;
-            map<int, int> cnt1,cnt2;
-            for (int i=0; i<word1.size(); i++) {
-                s1.insert(word1[i]);
-                m1[word1[i]]++;
-            }
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        
+        for (int i=0; i<word1.size(); i++) {
+            freq1[word1[i]-'a']++;
+        }
 
-            for (int i=0; i<word2.size(); i++) {
-                s2.insert(word2[i]);
-                m2[word2[i]]++;
-            }
+        for (int i=0; i<word2.size(); i++) {
+            freq2[word2[i]-'a']++;
+        }
 
-            for (auto itr=m1.begin(); itr!=m1.end(); itr++) {
-                cnt1[itr->second]++;
-            }
-
-            for (auto itr=m2.begin(); itr!=m2.end(); itr++) {
-                cnt2[itr->second]++;
-            }
-
-            for (auto itr = s1.begin(); itr!=s1.end(); itr++) {
-                if (s2.find(*itr)==s2.end()) {
-                    return false;
-                }
-            }
-            
-            for (auto itr = s2.begin(); itr!=s2.end(); itr++) {
-                if (s1.find(*itr)==s1.end()) {
-                    return false;
-                }
-            }
-
-            for (auto itr = cnt1.begin(); itr!=cnt1.end(); itr++) {
-                if (cnt2[itr->first]!=itr->second) {
-                    return false;
-                }
-            }
-
-            for (auto itr = cnt2.begin(); itr!=cnt2.end(); itr++) {
-                if (cnt1[itr->first]!=itr->second) {
-                    return false;
-                }
+        for (int i=0; i<26; i++) {
+            if ((freq1[i]==0 && freq2[i]!=0) || (freq1[i]!=0 && freq2[i]==0)) {
+                return false;
             }
         }
+
+        sort(freq1.begin(), freq1.end());
+        sort(freq2.begin(), freq2.end());
+
+        for (int i=0; i<26; i++) {
+            if (freq1[i]!=freq2[i]) {
+                return false;
+            }
+        }
+
         return true;
     }
 };
