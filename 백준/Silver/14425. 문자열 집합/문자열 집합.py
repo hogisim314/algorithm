759c844941c2a2fe6b1562833b9144f39fb0e642
hogisim314 <48578499+hogisim314@users.noreply.github.com>
2024-07-07 00:47:15 +0900

    [Silver IV] Title: 문자열 집합, Time: 116 ms, Memory: 36680 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+s=set()
+count = 0
+n, m = map(int, input().split())
+
+for _ in range(n):
+    data=input().rstrip()
+    s.add(data)
+
+for _ in range(m):
+    data=input().rstrip()
+    if data in s:
+        count+=1
+
+print(count)
