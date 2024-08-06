bcbe3cfa667839a32bd5a02e1b52fabe34c47b59
hogisim314 <hogisim314@gmail.com>
2024-08-06 09:34:45 +0900

    [Gold IV] Title: 최단경로, Time: 148 ms, Memory: 11456 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,59 +1,81 @@
 #include <iostream>
-#include <queue>
-#include <climits>
+#include <algorithm>
 #include <vector>
+#include <stdio.h>
 using namespace std;
-
-int V, E;
-int K;
-int u, v, w;
-
-const int INF = INT_MAX;
-
-const int MAX_V = 20'005;
-vector<pair<int, int> > Edges[MAX_V];//first가 거리
-vector<int> dist(MAX_V, INF);
-priority_queue<pair<int, int> > pq;
-
-void Dijkstra(int start) {
-	dist[start] = 0;
-	for (int i = 0; i < Edges[start].size(); i++) {
-		pq.push({-Edges[start][i].second, Edges[start][i].first });
-	}
-	while (!pq.empty()) {
-		int weight = -pq.top().first;
-		int now = pq.top().second;
-		pq.pop();
-		if (dist[now] != INF) continue;
-		dist[now] = weight;
-		for (int i = 0; i < Edges[now].size(); i++) {
-			if (dist[Edges[now][i].first] == INF) {
-				pq.push({ -(Edges[now][i].second + weight ), Edges[now][i].first});
-			}
-		}
-	}
+struct ss{
+    int d,en;
+};
+vector<ss> v[20001];
+int n,m,s,ans[20001],check[20001];
+struct data{
+    int no,d;
+}d[300100];
+bool cmp(data a, data b)
+{
+    return a.d>b.d;
 }
-
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> V >> E;
-	cin >> K;
-	for (int e = 1; e <= E; e++) {
-		cin >> u >> v >> w;
-		Edges[u].push_back({ v,w });
-	}
-	Dijkstra(K);
-
-	for (int v = 1; v <= V; v++) {
-		if (dist[v] == INF)
-		{
-			cout << "INF" << '\n';
-		}
-		else {
-			cout << dist[v] << '\n';
-		}
-	}
-	return 0;
+int main()
+{
+    cin>>n>>m;
+    cin>>s;
+    for(int i=1; i<=m; i++)
+    {
+        int st;
+        ss q;
+        scanf("%d %d %d",&st, &q.en, &q.d);
+        v[st].push_back(q);
+    }
+    for(int i=1; i<=n; i++)
+        ans[i]=1<<30;
+    d[1].d=0;
+    d[1].no=s;
+    ans[s]=0;
+    int cnt=2;
+    while(cnt>1)
+    {
+        int y=0;
+        int va,ed;
+        while(check[d[1].no]==1)
+        {
+            pop_heap(d+1,d+cnt,cmp);
+            cnt--;
+            if(cnt==1)
+            {
+                y=1;
+                break;
+            }
+        }
+        if(y)
+            break;
+        va=d[1].d;
+        ed=d[1].no;
+        pop_heap(d+1,d+cnt,cmp);
+        cnt--;
+        for(int i=0; i<v[ed].size(); i++)
+        {
+            if(check[v[ed][i].en]==1)
+                continue;
+            else
+            {
+                if(ans[v[ed][i].en]>va+v[ed][i].d)
+                {
+                    ans[v[ed][i].en]=va+v[ed][i].d;
+                    d[cnt].d=va+v[ed][i].d;
+                    d[cnt].no=v[ed][i].en;
+                    cnt++;
+                    push_heap(d+1,d+cnt,cmp);
+                }
+            }
+        }
+        check[ed]=1;
+    }
+    for(int i=1; i<=n; i++)
+    {
+        if(ans[i]==1<<30)
+            printf("INF\n");
+        else
+            printf("%d\n",ans[i]);
+    }
+    return 0;
 }
