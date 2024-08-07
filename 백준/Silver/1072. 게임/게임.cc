fa4b20ddc2523e2ffac6f62d7216aa2cce7372cb
hogisim314 <hogisim314@gmail.com>
2024-08-07 17:33:16 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -1,27 +1,26 @@
-#include <limits.h>
-
-#include <algorithm>
-#include <iostream>
+#include<iostream>
 using namespace std;
 long long x, y, z;
-long long ans = INT_MAX;
-int main() {
-    cin >> x >> y;
-    z = y * 100 / x;
 
-    long long lo = 0, hi = 1'000'000'500;
-    while (lo + 1 < hi) {
-        long long mid = lo + (hi - lo) / 2;
-        if ((y + mid) * 100 / (x + mid) >= z + 1) {
-            hi = mid;
-            ans = min(ans, mid);
-        } else {
-            lo = mid;
-        }
-    }
-    if (ans == INT_MAX) {
-        cout << -1;
-        return 0;
-    }
-    cout << ans;
+
+int main() {
+	cin >> x >> y;
+	z = y * 100 / x;
+
+	if (z >= 99) {
+		cout << "-1" << endl;
+		return 0;
+	}
+	int l = 0, r = x;
+
+	while (l < r) {
+		int piv = (l + r) / 2;
+		if ((long long)(y + piv) * 100 / (long long)(x + piv) > z) {
+			r = piv;
+		}
+		else {
+			l = piv + 1;
+		}
+	}
+	cout << r << endl;
 }
