8f1e107137809b1bb1dfca6b3556b051976743b7
hogisim314 <hogisim314@gmail.com>
2024-08-07 11:15:55 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 704 ms, Memory: 11664 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.cc
@@ -1,43 +1,43 @@
 #include <iostream>
-#include <queue>
-#include <stdio.h>
 #include <algorithm>
 using namespace std;
-int arr[1000][1000];
-int dp[1000][1000];
-int main()
-{
-	int n,m;
-	cin>>n>>m;
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++)
-			scanf("%1d",&arr[i][j]);
-	for(int i=0; i<n; i++)
-		dp[i][0]=arr[i][0];
-	for(int i=0; i<m; i++)
-		dp[0][i]=arr[0][i];
-	for(int i=1; i<n; i++)
-	{
-		for(int j=1; j<m; j++)
-		{
-			if(arr[i][j]==0)
-			{
-				dp[i][j]=0;
-				continue;
-			}
-			if(dp[i-1][j-1]<=dp[i-1][j] && dp[i-1][j-1]<=dp[i][j-1])
-				dp[i][j]=dp[i-1][j-1]+1;
-			else if(dp[i-1][j]<=dp[i][j-1] && dp[i-1][j]<=dp[i-1][j-1])
-				dp[i][j]=dp[i-1][j]+1;
-			else
-				dp[i][j]=dp[i][j-1]+1;
-		}
-	}
-	int maxx=0;
-	for(int i=0; i<n; i++)
-		for(int j=0; j<m; j++)
-			if(maxx<dp[i][j])
-				maxx=dp[i][j];
-	cout<<maxx*maxx<<endl;
-    return 0;
+int map[1111][1111];
+int psum[1111][1111];
+int n, m;
+int main() {
+  cin>>n>>m;
+  for (int i=1; i<=n; ++i) {
+    for (int j=1; j<=m; ++j) {
+      scanf("%1d", &map[i][j]);
+    }
+  }
+  for (int i=1; i<=n; ++i) {
+    for (int j=1; j<=m; ++j) {
+      psum[i][j]=psum[i-1][j]+psum[i][j-1]-psum[i-1][j-1]+map[i][j];
+    }
+  }
+
+  int ans=0;
+  for (int i=1; i<=n; ++i) {
+    for (int j=1; j<=m; ++j) {
+      //게임판의 숫자가 1이라면
+      if (map[i][j]==1) {
+        int res=1;
+        int idx=1;
+
+        while(i+idx<=n&&j+idx<=m) {
+          //특정 두 지점 사이의 부분 합
+          int space = psum[i+idx][j+idx]-psum[i+idx][j-1]-psum[i-1][j+idx]+psum[i-1][j-1];
+
+          idx++;
+
+          if (space!=idx*idx) break;
+
+          res=idx*idx;
+        }
+        ans=max(ans,res);
+      }
+    }
+  }
+  cout<<ans;
 }
