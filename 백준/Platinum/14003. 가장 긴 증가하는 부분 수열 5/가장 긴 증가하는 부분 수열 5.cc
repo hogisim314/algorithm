fef4539d30e3310b4aa2ddab1c26dc7d3c3e0fff
hogisim314 <hogisim314@gmail.com>
2024-08-07 15:55:09 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 184 ms, Memory: 20012 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,42 +1,43 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
 #include <vector>
-using namespace std;
 
+using namespace std;
 int arr[1000001];
-vector<int> v;
-int vector_idx[1000001];//vector_idx[i]는 arr[i]가 vector에 들어간 idx값.
+vector<int> K;
+int Index[1000001];
 int ans[1000001];
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	int n;
-	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
-	v.push_back(arr[0]);
-	vector_idx[0] = 0;
-	for (int i = 1; i < n; i++) {
-		int back = v.back();
-		if (arr[i] > back) {
-			v.push_back(arr[i]);
-			vector_idx[i] = v.size()-1;
-		}
-		else {
-			int k = lower_bound(v.begin(), v.end(), arr[i]) - v.begin();
-			v[k] = arr[i];
-			vector_idx[i] = k;
-		}
-	}
-	cout << v.size()<< '\n';
-	int k = v.size()-1;
-	for (int i = n-1; i >= 0; i--) {
-		if (vector_idx[i] == k) {
-			ans[k] = arr[i]; k--;
-		}
-	}
-	for (int i = 0; i < v.size(); i++) {
-		cout << ans[i] << ' ';
-	}
+int target;
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int idx = 0;
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+        if (K.empty()) {
+            K.push_back(arr[i]);
+        }
+        if (K[K.size() - 1] < arr[i]) {
+            K.push_back(arr[i]);
+            Index[i] = K.size() - 1;
+        } else {
+            idx = lower_bound(K.begin(), K.end(), arr[i]) - K.begin();
+            K[idx] = arr[i];
+            Index[i] = idx;
+        }
+    }
+    int target = K.size() - 1;
+    for (int i = n; i >= 1; i--) {
+        if (Index[i] == target) {
+            ans[target] = arr[i];
+            target--;
+            if (target == -1) break;
+        }
+    }
+    cout << K.size() << '\n';
+    for (int i = 0; i <= K.size() - 1; i++) {
+        cout << ans[i] << ' ';
+    }
 }
