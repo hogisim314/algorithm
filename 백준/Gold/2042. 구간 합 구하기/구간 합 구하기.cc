b4fd343ad136ae4917b4a804f54447519131c3d1
hogisim314 <hogisim314@gmail.com>
2024-08-14 00:01:09 +0900

    [Gold I] Title: 구간 합 구하기, Time: 228 ms, Memory: 26240 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -1,65 +1,68 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cmath>
+#include <vector>
+#include<cmath>
 using namespace std;
-#define ll long long
+long long n, m, k;
+int tree_size, h;
 
-ll init(vector<ll> &arr, vector<ll> &tree, ll node, ll start, ll end) {
-  if (start==end) return tree[node]=arr[start];
 
-  ll mid=(start+end)/2;
-  //구간 합을 구하는 경우
-  return tree[node]=init(arr, tree, node*2, start, mid)+init(arr, tree, node*2+1, mid+1, end);
+long long init(vector<long long> &arr, vector<long long> &tree, long long node, long long start, long long end) {
+	if (start == end)
+		return tree[node] = arr[start];
+
+	long long mid = (start + end) / 2;
+
+	return tree[node] = init(arr, tree, node * 2, start, mid) + init(arr, tree, node * 2 + 1, mid + 1, end);
 }
 
-ll sum(vector<ll> &tree, ll node, ll start, ll end, ll left, ll right) {
-  //case 1: 교집합이 공집합
-  if (right<start||left>end) return 0;
+long long sum(vector<long long> &tree, long long node, long long start, long long end, long long left, long long right) {
+	if (left > end || right < start) return 0;
 
-  //case 2: [start, end]가 [left, right]에 포함
-  if (left<=start&&end<=right) return tree[node];
+	if (left <= start && end <= right) return tree[node];
 
-  //case 3, 4: 왼쪽 자식과 오른쪽 자식을 루트로 하는 트리에서 다시 탐색 시작
-  ll mid=(start+end)/2;
-  return sum(tree, node*2, start, mid, left, right)+sum(tree, node*2+1, mid+1, end, left, right);
+	long long mid = (start+end) / 2;
+	return sum(tree, node * 2, start, mid, left, right) + sum(tree, node * 2 + 1, mid + 1, end, left, right);
 }
 
-void update(vector<ll> &tree, ll node, ll start, ll end, ll index, ll diff) {
-  if (index<start||index>end) return;
-  tree[node]=tree[node]+diff;
+void update(vector<long long> &tree, long long node, long long start, long long end, long long index, long long diff) {//diff = val - tree[index]
+	if (index<start || index>end) return;
+	tree[node] = tree[node] + diff;
 
-  if (start!=end) {
-    ll mid=(start+end)/2;
-    update(tree,node*2, start, mid, index,diff);
-    update(tree,node*2+1, mid+1,end, index, diff);
-  }
+	if (start != end) {
+		long long mid = (start + end) / 2;
+		update(tree, node * 2, start, mid, index, diff);
+		update(tree, node * 2 + 1, mid + 1, end, index, diff);
+	}
 }
 
 int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(0);
-  ll N, M, K;
-  cin>>N>>M>>K;
-  vector<long long> arr(N);
-  int h= (int)ceil(log2(N));
-  int tree_size=(1<<(h+1));
-  vector<long long> tree(tree_size);
-  for (int i=0; i<N; ++i) {
-    cin>>arr[i];
-  }
-  init(arr, tree, 1, 0, N-1);
-  for (int i=0; i<M+K; ++i) {
-    ll a, b, c;
-    cin>>a>>b>>c;
-    if (a==1) {
-      b--;
-      long long diff=c-arr[b];
-      arr[b]=c;
-      update(tree, 1, 0, N-1, b, diff);
-    }
-    else if (a==2) {
-      cout<<sum(tree, 1, 0,N-1, b-1, c-1)<<'\n';
-    }
-  }
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> n >> m >> k;
+	h = (int)ceil(log2(n));
+	vector<long long> arr(n);
+	tree_size = (1 << (h + 1));
+	vector<long long> tree(tree_size);
+	for (int i = 0; i <= n-1; i++) {
+		long long num;
+		cin >> num;
+		arr[i] = num;
+	}
+
+	init(arr, tree, 1,0,n-1);
+
+	for (int i = 0; i < m+k; i++) {
+		long long a, b, c;
+		cin >> a >> b >> c;
+		if (a == 1) {//update
+			b--;
+			long long diff = c - arr[b];
+			arr[b] = c;
+			update(tree, 1, 0,n-1 , b,diff);
+		}
+		else if (a == 2) {//sum
+			cout << sum(tree, 1,0,n-1, b-1, c-1) << '\n';
+		}
+	}
 }
