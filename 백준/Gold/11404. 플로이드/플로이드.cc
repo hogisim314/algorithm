1ec7556e4c64cd370fb6af33c7c220bf7676a61b
hogisim314 <hogisim314@gmail.com>
2024-08-15 11:58:36 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -1,50 +1,49 @@
-#include <limits.h>
-#include <math.h>
-
 #include <algorithm>
 #include <iostream>
-#include <vector>
 using namespace std;
 int n, m;
 int a, b, c;
-vector<pair<int, int> > Edges[111];
-long long dp[111][111];
+int dist[111][111];
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    cin >> n;
+    cin >> m;
 
-    cin >> n >> m;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (i == j) {
-                dp[i][j] = 0;
-                continue;
-            }
-            dp[i][j] = INT_MAX;
+            if (i == j) continue;
+            dist[i][j] = 210000000;
         }
     }
+
     for (int i = 1; i <= m; i++) {
         cin >> a >> b >> c;
-        if (dp[a][b] > c) {
-            dp[a][b] = c;
-        }
+        dist[a][b] = min(dist[a][b], c);
     }
+
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         cout << dist[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    // cout << '\n';
 
     for (int k = 1; k <= n; k++) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
-                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j]);
+                dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
             }
         }
     }
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if (dp[i][j] == INT_MAX) {
+            if (dist[i][j] == 210000000) {
                 cout << 0 << ' ';
-                continue;
-            }
-            cout << dp[i][j] << ' ';
+            } else
+                cout << dist[i][j] << ' ';
         }
         cout << '\n';
     }
