cd6bae58fccbaab5208fda3f7b5c7ef4da73b95d
hogisim314 <hogisim314@gmail.com>
2024-08-16 10:27:24 +0900

    [Gold V] Title: 집합의 표현, Time: 32 ms, Memory: 6324 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.cc
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.cc
@@ -1,46 +1,42 @@
-#include<iostream>
-#include<stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-
 int n, m;
-int parent[1000001];
+int cmd, a, b;
+int parent[1000100];
 
-int find_node(int now) {
-	if (parent[now] == now) {
-		return now;
-	}
-	else {
-		return parent[now] = find_node(parent[now]);
-	}
+int Find(int k) {  // 조상을 찾는
+    if (parent[k] == k) {
+        return k;
+    }
+    return parent[k] = Find(parent[k]);
 }
 
-int main()
-{
-	cin >> n >> m;
+void UNION(int a, int b) {
+    int ancestorA = Find(a);
+    int ancestorB = Find(b);
+    parent[ancestorA] = ancestorB;
+}
 
-	for (int i = 1; i <= n; i++) {
-		parent[i] = i;
-	}
-
-	for (int i = 0; i < m; i++) {
-		int cmd, a, b;
-		scanf("%d %d %d", &cmd, &a, &b);
-
-		if (cmd == 0) {
-			int aRoot = find_node(a);
-			int bRoot = find_node(b);
-			parent[bRoot] = aRoot;
-		}
-
-		if (cmd == 1) {
-			int visited[1000001] = { 0, };
-			if (find_node(b) == find_node(a)) {
-				printf("YES\n");
-			}
-			else {
-				printf("NO\n");
-			}
-		}
-	}
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> m;
+    for (int i = 1; i <= n; i++) {
+        parent[i] = i;
+    }
+    for (int i = 1; i <= m; i++) {
+        cin >> cmd >> a >> b;
+        if (cmd == 0) {
+            // 합집합
+            UNION(a, b);
+        } else if (cmd == 1) {
+            // 같은집합이니?
+            if (Find(a) == Find(b)) {
+                cout << "YES" << '\n';
+            } else
+                cout << "NO" << '\n';
+        }
+    }
 }
