b65dc6eab5d93120bb660588817491715bbe904d
hogisim314 <hogisim314@gmail.com>
2024-08-26 12:43:27 +0900

    [Gold III] Title: ACM Craft, Time: 476 ms, Memory: 2724 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -3,57 +3,66 @@
 #include <algorithm>
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 int tc;
 int n, k;
-int cost_time[1111];
 int x, y, w;
-int dp[1111];  // dp[i]는 i번 건물이 지어지는 최소시간.
+int timecost[1111];
+int dptime[1111];
+vector<int> nodes[1111];
 int indegree[1111];
+int mintime;
+
+void topological_sort() {
+    queue<int> q;
+    for (int i = 1; i <= n; i++) {
+        if (indegree[i] == 0) {
+            q.push(i);
+            dptime[i] = timecost[i];
+        }
+    }
+
+    while (!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        for (int i = 0; i < nodes[cur].size(); i++) {
+            int adj = nodes[cur][i];
+            indegree[adj]--;
+            if (indegree[adj] == 0) {
+                q.push(adj);
+            }
+            dptime[adj] = max(dptime[adj], dptime[cur] + timecost[adj]);
+        }
+    }
+}
 
 int main() {
     cin >> tc;
     while (tc--) {
-        memset(cost_time, 0, sizeof(cost_time));
-        memset(dp, -1, sizeof(dp));
+        memset(timecost, 0, sizeof(timecost));
+        memset(dptime, 0, sizeof(dptime));
         memset(indegree, 0, sizeof(indegree));
-        vector<int> nextBuilding[1111];
-        vector<int> dep[1111];
-        queue<int> q;
-        int top, neighbor;
         cin >> n >> k;
         for (int i = 1; i <= n; i++) {
-            cin >> cost_time[i];
+            cin >> timecost[i];
         }
         for (int i = 1; i <= k; i++) {
             cin >> x >> y;
+            nodes[x].push_back(y);
             indegree[y]++;
-            nextBuilding[x].push_back(y);  // x 짓고 나서 지어야 하는 건물.
         }
         cin >> w;
+        topological_sort();
 
+        // cout << "debug" << '\n';
+        // for (int i = 1; i <= n; i++) {
+        //     cout << dptime[i] << ' ';
+        // }
+        // cout << '\n';
+
+        cout << dptime[w] << '\n';
         for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-                dp[i] = cost_time[i];
-            }
+            nodes[i].clear();
         }
-
-        while (!q.empty()) {
-            top = q.front();
-            q.pop();
-
-            for (int i = 0; i < nextBuilding[top].size(); i++) {
-                neighbor = nextBuilding[top][i];
-                dp[neighbor] = max(dp[top] + cost_time[neighbor], dp[neighbor]);
-                indegree[neighbor]--;
-                if (indegree[neighbor] == 0) {
-                    q.push(neighbor);
-                }
-            }
-        }
-
-        cout << dp[w] << '\n';
     }
 }
