3aec43c1f26274265802d373e43c46167d80d3ba
hogisim314 <hogisim314@gmail.com>
2024-08-27 13:31:20 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 112 ms, Memory: 9844 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -1,97 +1,102 @@
+#include <limits.h>
+
+#include <algorithm>
 #include <iostream>
 #include <queue>
-#include <climits>
 #include <vector>
 using namespace std;
+int n, e;
+int a, b, c;
+int v1, v2;
+vector<pair<int, int> > nodes[888];
+int dist[888];
+bool visited[888];
+int adj, weight;
+int cur;
+int ans1, ans2;
 
-long long N, E;
-long long a, b, c;
-long long v1, v2;
-long long road_1_v1;
-long long road_1_v2;
-long long road_v1_v2;
-long long road_v1_N;
-long long road_v2_v1;
-long long road_v2_N;
-long long firstroad;
-long long secondroad;
-const long long INF = LLONG_MAX;
+void debug() {
+    for (int i = 1; i <= n; i++) {
+        cout << dist[i] << ' ';
+    }
+    cout << '\n';
+}
 
-const long long MAX_V = 805;
-vector<pair<long long, long long> > Edges[MAX_V];//first가 거리
-vector<long long> dist(MAX_V, INF);
-priority_queue<pair<long long, long long> > pq;
-long long maxx = 0;
+void init() {
+    for (int i = 1; i <= n; i++) {
+        dist[i] = INT_MAX;
+        visited[i] = false;
+    }
+}
 
-void Dijkstra(long long start);
+void Dijkstra(int st) {
+    priority_queue<pair<int, int> > pq;
+    visited[st] = true;
+    dist[st] = 0;
+    for (int i = 0; i < nodes[st].size(); i++) {
+        adj = nodes[st][i].first;
+        weight = nodes[st][i].second;
+        pq.push(make_pair(-weight, adj));
+    }
+
+    while (!pq.empty()) {
+        weight = -pq.top().first;
+        cur = pq.top().second;
+        pq.pop();
+        if (dist[cur] != INT_MAX) continue;
+        visited[cur] = true;
+        dist[cur] = weight;
+        for (int i = 0; i < nodes[cur].size(); i++) {
+            adj = nodes[cur][i].first;
+            weight = nodes[cur][i].second;
+            if (visited[adj]) continue;
+            if (dist[adj] > dist[cur] + weight) {
+                pq.push(make_pair(-(dist[cur] + weight), adj));
+            }
+        }
+    }
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n >> e;
+    for (int i = 1; i <= e; i++) {
+        cin >> a >> b >> c;
+        nodes[a].push_back(make_pair(b, c));
+        nodes[b].push_back(make_pair(a, c));
+    }
+    cin >> v1 >> v2;
 
-	cin >> N >> E;
-	for (long long e = 1; e <= E; e++) {
-		cin >> a >> b >> c;
-		Edges[a].push_back({b,c });
-		Edges[b].push_back({ a,c });
-	}
-	cin >> v1 >> v2;
+    init();
+    Dijkstra(1);
+    if (dist[v1] == INT_MAX || dist[v2] == INT_MAX) {
+        cout << -1;
+        return 0;
+    }
+    ans1 += dist[v1];
+    ans2 += dist[v2];
 
-	Dijkstra(1);
-	road_1_v1 = dist[v1];
-	road_1_v2 = dist[v2];
+    init();
+    Dijkstra(v1);
+    if (dist[v2] == INT_MAX || dist[n] == INT_MAX) {
+        cout << -1;
+        return 0;
+    }
+    ans1 += dist[v2];
+    ans2 += dist[n];
 
-	for (long long i = 0; i < dist.size(); i++) {
-		dist[i] = INF;
-	}
-	Dijkstra(v1);
-	road_v1_v2 = dist[v2];
-	road_v1_N = dist[N];
+    init();
+    Dijkstra(v2);
+    if (dist[n] == INT_MAX || dist[v1] == INT_MAX) {
+        cout << -1;
+        return 0;
+    }
+    ans1 += dist[n];
+    ans2 += dist[v1];
 
-	for (long long i = 0; i < dist.size(); i++) {
-		dist[i] = INF;
-	}
-	Dijkstra(v2);
-	road_v2_v1 = dist[v1];
-	road_v2_N = dist[N];
-	
-	//check road 1->v1->v2->E
-	if (road_1_v1 != INF && road_v1_v2 != INF && road_v2_N != INF) {
-		firstroad += road_1_v1;
-		firstroad += road_v1_v2;
-		firstroad += road_v2_N;
-	}
-
-	if (road_1_v2 != INF && road_v2_v1 != INF && road_v1_N != INF) {
-		secondroad += road_1_v2;
-		secondroad += road_v2_v1;
-		secondroad += road_v1_N;
-	}
-
-	//1->v1->v2->n
-	//1->v2->v1->n
-	if (firstroad != 0 && secondroad != 0) {
-		cout << min(firstroad, secondroad);
-	}
-	else cout << -1;
-	return 0;
+    cout << min(ans1, ans2);
 }
 
-void Dijkstra(long long start) {
-	dist[start] = 0;
-	for (long long i = 0; i < Edges[start].size(); i++) {
-		pq.push({ -Edges[start][i].second, Edges[start][i].first });
-	}
-	while (!pq.empty()) {
-		long long weight = -pq.top().first;
-		long long now = pq.top().second;
-		pq.pop();
-		if (dist[now] != INF) continue;
-		dist[now] = weight;
-		for (long long i = 0; i < Edges[now].size(); i++) {
-			if (dist[Edges[now][i].first] == INF) {
-				pq.push({ -(Edges[now][i].second + weight), Edges[now][i].first });
-			}
-		}
-	}
-}
+// 1->v1->v2->N
+// 1->v2->v1->N
