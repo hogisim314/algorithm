2e093b2d6fdd12e503f6ff8580fa944e08eb9043
hogisim314 <hogisim314@gmail.com>
2024-08-28 11:19:05 +0900

    [Platinum IV] Title: 달리기, Time: 300 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Platinum/2517. 달리기/달리기.cc
+++ 백준/Platinum/2517. 달리기/달리기.cc
@@ -1,66 +1,72 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
-int n, h, tree_size;
+typedef long long ll;
+int n;
+int fast;
+int idx;
+vector<pair<int, int>> order;
+vector<int> result(500500);
+int h, tree_size;
 
-bool custom_comp(pair<int, int> a, pair<int, int> b) {
-    if (a.second < b.second) {
-        return true;
-    } else
-        return false;
+int sum(vector<int> &tree, int node, int start, int end, int left, int right) {
+    if (right < start || end < left) {
+        return 0;
+    }
+    if (left <= start && end <= right) {
+        return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    return sum(tree, node * 2, start, mid, left, right) +
+           sum(tree, node * 2 + 1, mid + 1, end, left, right);
 }
-
 void update(vector<int> &tree, int node, int start, int end, int index,
             int diff) {
-    if (index < start || end < index) return;
+    if (index < start || end < index) {
+        return;
+    }
     tree[node] += diff;
     if (start != end) {
-        long long mid = (start + end) / 2;
+        int mid = (start + end) / 2;
         update(tree, node * 2, start, mid, index, diff);
         update(tree, node * 2 + 1, mid + 1, end, index, diff);
     }
 }
 
-int query(vector<int> &tree, int node, int start, int end, int left,
-          int right) {
-    if (end < left || right < start) return 0;
-    if (left <= start && end <= right) return tree[node];
-
-    long long mid = (start + end) / 2;
-    return query(tree, node * 2, start, mid, left, right) +
-           query(tree, node * 2 + 1, mid + 1, end, left, right);
+void debug(vector<int> &tree) {
+    for (int i = 0; i <= tree_size; i++) {
+        cout << tree[i] << ' ';
+    }
+    cout << '\n';
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cin >> n;
-    vector<pair<int, int> > arr(n);
-    for (int i = 0; i < n; i++) {
-        arr[i].first = i;
-        cin >> arr[i].second;
-    }
-
     h = (int)ceil(log2(n));
     tree_size = 1 << (h + 1);
     vector<int> tree(tree_size);
-    vector<pair<int, int> > result(n);
-    sort(arr.begin(), arr.end(), custom_comp);
-
-    for (int i = 0; i < n; i++) {
-        arr[i].second = i;
+    for (int i = 1; i <= n; i++) {
+        cin >> fast;
+        order.push_back(make_pair(fast, i));
+    }
+    sort(order.begin(), order.end(), greater<pair<int, int>>());
+    // debug(tree);
+    for (int i = 0; i < order.size(); i++) {
+        fast = order[i].first;
+        idx = order[i].second;
+        update(tree, 1, 1, n, idx, 1);
+        // debug(tree);
+        result[idx] = sum(tree, 1, 1, n, 1, idx - 1) + 1;
+        // cout << result[idx] << '\n';
     }
 
-    for (int i = n - 1; i >= 0; i--) {
-        result[i].first = arr[i].first;
-        result[i].second = query(tree, 1, 0, n - 1, 0, arr[i].first - 1) + 1;
-        update(tree, 1, 0, n - 1, arr[i].first, 1);
-    }
-
-    sort(result.begin(), result.end());
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i].second << '\n';
+    for (int i = 1; i <= n; i++) {
+        cout << result[i] << '\n';
     }
 }
