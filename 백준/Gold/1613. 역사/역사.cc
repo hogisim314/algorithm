09b027ca4610355e08d94b806f104c9852ba4205
hogisim314 <hogisim314@gmail.com>
2024-08-29 18:41:35 +0900

    [Gold III] Title: 역사, Time: 68 ms, Memory: 2792 KB -BaekjoonHub

--- 백준/Gold/1613. 역사/역사.cc
+++ 백준/Gold/1613. 역사/역사.cc
@@ -26,6 +26,7 @@ int main() {
     cin >> n >> k;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
+            if (i == j) continue;
             dist[i][j] = INF;
         }
     }
