f00881d3f6fb451a4d6d490094ecfa168062064f
hogisim314 <hogisim314@gmail.com>
2024-08-30 09:44:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 44 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,56 +1,55 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
+int V, E, A, B, C;
+int weight;
+int v1, v2;
+int parent[10100];
+long long ans;
+priority_queue<pair<int, pair<int, int>>> pq;
 
-int V, E;
-int A, B, C;
-
-const int MAX_V = 10'004;
-priority_queue<pair<int, pair<int, int>> > pq;
-
-int parent[MAX_V];
-int Find(int x) {
-	if (x == parent[x]) {
-		return x;
-	}
-	return parent[x] = Find(parent[x]);
+int Find(int u) {
+    if (parent[u] == u) return u;
+    return parent[u] = Find(parent[u]);
 }
 
-void Union(int x, int y) {
-	parent[Find(x)] = Find(y);
+void Union(int u, int v) {
+    int pu = Find(u);
+    int pv = Find(v);
+
+    if (pu == pv) return;
+    parent[pu] = pv;
 }
 
-int Kruskal(void) {
-	int dab = 0;
-	while (!pq.empty())
-	{
-		int u = pq.top().first;
-		int v = pq.top().second.first;
-		int w = pq.top().second.second;
-		pq.pop();
-		if (Find(v) == Find(w)) 
-		{
-			continue;
-		}
-		else 
-		{
-			dab += -u;
-			Union(v, w);
-		}
-	}
-	return dab;
+void MST() {
+    while (!pq.empty()) {
+        weight = -pq.top().first;
+        v1 = pq.top().second.first;
+        v2 = pq.top().second.second;
+
+        pq.pop();
+
+        if (Find(v1) != Find(v2)) {
+            ans += weight;
+            Union(v1, v2);
+        } else
+            continue;
+    }
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
-	cin >> V >> E;
-	for (int v = 1; v <= V; v++) {
-		parent[v] = v;
-	}
-	for (int e = 1; e <= E; e++) {
-		cin >> A >> B >> C;
-		pq.push({ -C,{A,B} });
-	}
-	cout << Kruskal() << '\n';
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> V >> E;
+    for (int i = 1; i <= V; i++) {
+        parent[i] = i;
+    }
+    for (int i = 1; i <= E; i++) {
+        cin >> A >> B >> C;
+        pq.push(make_pair(-C, make_pair(A, B)));
+    }
+    MST();
+    cout << ans;
 }
