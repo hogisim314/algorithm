c16f34b1c4411774a499e2a3b6709090a20464c1
hogisim314 <hogisim314@gmail.com>
2024-09-02 16:46:08 +0900

    [Silver V] Title: 줄세우기, Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.cc
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -3,45 +3,21 @@
 #include <queue>
 using namespace std;
 int P;
-int sum;
-int kids[22];
+int result;
+int students[22];
 int h;
 int idx;
 int main() {
     cin >> P;
     for (int p = 1; p <= P; p++) {
-        sum = 0;
-
-        for (int i = 1; i <= 20; i++) {
-            kids[i] = 0;
-        }
-
+        result = 0;
         cin >> idx;
-        cin >> h;
-        kids[1] = h;
-
-        for (int i = 2; i <= 20; i++) {
-            cin >> h;
-            bool change = false;
-            for (int j = 1; j <= i - 1; j++) {
-                if (kids[j] > h) {
-                    for (int k = i - 1; k >= j; k--) {
-                        kids[k + 1] = kids[k];
-                    }
-                    sum += (i - j);
-                    change = true;
-                    kids[j] = h;
-                    break;
-                } else
-                    continue;
+        for (int i = 0; i < 20; i++) cin >> students[i];
+        for (int i = 0; i < 20; i++) {
+            for (int j = i; j < 20; j++) {
+                if (students[i] > students[j]) result += 1;
             }
-            if (!change) kids[i] = h;
-            // cout << "debug" << ' ';
-            // for (int j = 1; j <= 20; j++) {
-            //     cout << kids[j] << ' ';
-            // }
-            // cout << '\n';
         }
-        cout << p << ' ' << sum << '\n';
+        cout << p << ' ' << result << '\n';
     }
 }
