8ac2a7b84c5e779f18f0153d1b054489ab24446e
hogisim314 <hogisim314@gmail.com>
2024-09-13 14:53:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 2,946 ms, Memory: 38,304 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.cpp
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.cpp
@@ -15,7 +15,7 @@ double e;
 vector<pair<int, int> > Pos;
 int parent[1111];
 priority_queue<pair<double, pair<int, int> > > pq;
-vector<pair<int, int> > adj[1111];
+vector<pair<int, double> > adj[1111];
 double ans = 0;
 long long realans = 0;
 double dist;
@@ -35,7 +35,8 @@ void Union(int u, int v) {
     parent[pv] = pu;
 }
 
-void prim() {
+void kruskal() {
+    int cnt = 0;
     while (!pq.empty()) {
         dist = -pq.top().first;
         node1 = pq.top().second.first;
@@ -44,23 +45,64 @@ void prim() {
         if (Find(node1) == Find(node2)) continue;
         Union(node1, node2);
         ans += dist;
+        cnt++;
+        if (cnt >= n - 1) break;
     }
 }
 
-void link() {
+void prim(int start) {
+    for (int i = 0; i < adj[start].size(); i++) {
+        node1 = adj[start][i].first;
+        dist = adj[start][i].second;
+        pq.push(make_pair(-dist, make_pair(start, node1)));
+    }
+
+    while (!pq.empty()) {
+        dist = -pq.top().first;
+        node1 = pq.top().second.first;
+        node2 = pq.top().second.second;
+        pq.pop();
+        if (Find(node1) == Find(node2)) continue;
+        Union(node1, node2);
+        ans += dist;
+
+        for (int i = 0; i < adj[node2].size(); i++) {
+            node1 = adj[node2][i].first;
+            if (Find(node1) == Find(node2)) continue;
+            dist = adj[node2][i].second;
+            pq.push(make_pair(-dist, make_pair(node2, node1)));
+        }
+    }
+}
+
+void link_for_kruskal() {
     for (int i = 0; i < Pos.size(); i++) {
         for (int j = i + 1; j < Pos.size(); j++) {
             dist = pow(Pos[i].first - Pos[j].first, 2) +
                    pow(Pos[i].second - Pos[j].second, 2);
+
             pq.push(make_pair(-dist * e, make_pair(i, j)));
             pq.push(make_pair(-dist * e, make_pair(j, i)));
         }
     }
-    // cout << "pqsize " << pq.size() << '\n';
+}
+
+void link_for_prim() {
+    for (int i = 0; i < Pos.size(); i++) {
+        for (int j = i + 1; j < Pos.size(); j++) {
+            dist = pow(Pos[i].first - Pos[j].first, 2) +
+                   pow(Pos[i].second - Pos[j].second, 2);
+            adj[i].emplace_back(j, dist * e);
+            adj[j].emplace_back(i, dist * e);
+        }
+    }
 }
 
 void init() {
     Pos.clear();
+    for (int i = 0; i <= 1000; i++) {
+        adj[i].clear();
+    }
     memset(parent, 0, sizeof(parent));
     while (!pq.empty()) {
         pq.pop();
@@ -86,8 +128,9 @@ int main() {
         }
         cin >> e;
 
-        link();
-        prim();
+        link_for_kruskal();
+        kruskal();
+
         cout << fixed;
         cout.precision(0);
 
